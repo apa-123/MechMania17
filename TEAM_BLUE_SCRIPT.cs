fe984beecd67cb71ed3b6ecdcaae28007eb5e4e5
shreyshrey1 <talk2shreyas@gmail.com>
2017-09-30 02:04:28 -0500

    ltes

--- TEAM_BLUE_SCRIPT.cs
+++ TEAM_BLUE_SCRIPT.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 //---------- CHANGE THIS NAME HERE -------
 public class TEAM_BLUE_SCRIPT : MonoBehaviour
 {
+    public ObjectiveScript middleObjective;
+    public ObjectiveScript leftObjective;
+    public ObjectiveScript rightObjective;
+    public float timer = 0;
     //---------- CHANGE THIS NAME HERE -------
     public static TEAM_BLUE_SCRIPT AddYourselfTo(GameObject host) {
         //---------- CHANGE THIS NAME HERE -------
@@ -21,9 +27,15 @@ public class TEAM_BLUE_SCRIPT : MonoBehaviour
 
     void Start()
     {
+        // populate the objectives
+        middleObjective = GameObject.Find("MiddleObjective").GetComponent<ObjectiveScript>();
+        leftObjective = GameObject.Find("LeftObjective").GetComponent<ObjectiveScript>();
+        rightObjective = GameObject.Find("RightObjective").GetComponent<ObjectiveScript>();
+
         character1 = transform.Find("Character1").gameObject.GetComponent<CharacterScript>();
         character2 = transform.Find("Character2").gameObject.GetComponent<CharacterScript>();
         character3 = transform.Find("Character3").gameObject.GetComponent<CharacterScript>();
+
     }
     /*^^^^ DO NOT MODIFY ^^^^*/
 
@@ -31,27 +43,60 @@ public class TEAM_BLUE_SCRIPT : MonoBehaviour
     // Update() is called every frame
     void Update()
 	{
-        // Debug.Log(character1.name + " " + character1.);
-        //character1.FaceClosestWaypoint();
-        character1.SetFacing(character1.FindClosestObjective());
-        character2.SetFacing(character2.FindClosestObjective());
-        character3.SetFacing(character3.FindClosestObjective());
-
-        //set loadouts for each character
+         //Set caracter loadouts, can only happen when the characters are at base.
         if (character1.getZone() == zone.BlueBase || character1.getZone() == zone.RedBase)
-            character1.setLoadout(loadout.MEDIUM);
+            character1.setLoadout(loadout.LONG);
         if (character2.getZone() == zone.BlueBase || character2.getZone() == zone.RedBase)
             character2.setLoadout(loadout.LONG);
         if (character2.getZone() == zone.BlueBase || character2.getZone() == zone.RedBase)
             character3.setLoadout(loadout.LONG);
 
-        character2.MoveChar(new Vector3(5.0f, 5.0f, 5.0f));
-        character3.MoveChar(new Vector3(5.0f, 5.0f, 5.0f));
-        character1.MoveChar(new Vector3(5.0f, 5.0f, 5.0f));
-        Debug.Log(character1.FindClosestObjective().magnitude);
-        if (character1.FindClosestObjective().magnitude<=0.0f)
+        // in the first couple of seconds we just scan around
+        if (timer < 10)
         {
-            character1.MoveChar(new Vector3(0.0f, 0.0f, 0.0f));
+            character1.FaceClosestWaypoint();
+            character2.FaceClosestWaypoint();
+            character3.FaceClosestWaypoint();
+            character1.MoveChar(new Vector3(-8.8f, 1.5f, 13.5f));
+        }
+        // place sniper in position, run to cover if attacked
+        if (character1.attackedFromLocations.Capacity == 0)
+        {
+            character1.MoveChar(new Vector3(-8.8f, 1.5f, 13.5f));
+            character1.SetFacing(middleObjective.transform.position);
+        }
+        else
+        {
+            character1.MoveChar(character1.FindClosestCover(character1.attackedFromLocations[0]));
+        }
+        // send other two to capture
+        if (middleObjective.getControllingTeam() != character1.getTeam())
+        {
+            character2.MoveChar(leftObjective.transform.position);
+            character2.SetFacing(leftObjective.transform.position);
+            character2.FindClosestCover()
+            character3.MoveChar(rightObjective.transform.position);
+            character3.SetFacing(rightObjective.transform.position);
+            character3.FindClosestCover()
+        }
+        else
+        {
+            // Then left
+            if (leftObjective.getControllingTeam() != character1.getTeam())
+            {
+                character2.MoveChar(leftObjective.transform.position);
+                character2.SetFacing(leftObjective.transform.position);
+                character3.MoveChar(leftObjective.transform.position);
+                character3.SetFacing(leftObjective.transform.position);
+            }
+            // Then RIght
+            if (rightObjective.getControllingTeam() != character1.getTeam())
+            {
+                character2.MoveChar(rightObjective.transform.position);
+                character2.SetFacing(rightObjective.transform.position);
+                character3.MoveChar(rightObjective.transform.position);
+                character3.SetFacing(rightObjective.transform.position);
+            }
         }
 
     } 
